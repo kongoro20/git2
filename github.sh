d76a467371d3ce6abfcc8025d2e76dc8980440d9
kongoro20 <kongo6411@gmail.com>
2025-03-20 12:09:01 +0100

    Update github.sh

--- github.sh
+++ github.sh
@@ -5,6 +5,8 @@ python codespace.py
 python boss.py
 python newtab.py
 python boss.py
-xdotool mousemove 1358 11 click 1
-sleep 0.3
-xdotool mousemove 1358 11 click 1
+sleep 2
+while wmctrl -l | grep -i "Mozilla Firefox" >/dev/null; do
+    wmctrl -c "Mozilla Firefox"
+    sleep 0.5
+done
