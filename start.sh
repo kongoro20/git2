03ec52ea4521b190f51b6120a4327dcac8efa12d
kongoro20 <kongo6411@gmail.com>
2025-03-21 10:43:41 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@
 echo "Starting run.sh..."
 source run.sh
 echo "Finished run.sh. Sleeping for 4 hours..."
-sleep 14500  # 4 hours (3600 seconds * 4)
+sleep 12600  # 3:30 hours (3.5 hours)
 
 # Execute boocle.sh 100 times with 4-hour sleep between each execution
 for i in {1..100}; do
     echo "Execution $i of boocle.sh..."
     bash boocle.sh
     echo "Finished execution $i of boocle.sh. Sleeping for 4 hours..."
-    sleep 14500  # 4 hours
+    sleep 12600  # 3:30 hours (3.5 hours)
 done
 
 echo "All tasks completed!"
